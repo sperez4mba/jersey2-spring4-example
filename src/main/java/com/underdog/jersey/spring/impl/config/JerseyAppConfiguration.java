95d8ad79c2a35f5ab88965b494caf49addd12217
Brice Dutheil <brice.dutheil@gmail.com>
2016-07-25 23:15:49 +0200

    Made work the resource unit test

--- src/main/java/com/underdog/jersey/spring/impl/config/JerseyAppConfiguration.java
+++ src/main/java/com/underdog/jersey/spring/impl/config/JerseyAppConfiguration.java
@@ -2,9 +2,7 @@
 package com.underdog.jersey.spring.impl.config;
 
 import java.time.Instant;
-import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Context;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
@@ -14,11 +12,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class JerseyAppConfiguration extends ResourceConfig {
 
     public JerseyAppConfiguration() {
-    }
-
-    @Context
-    public void post_construct(ServletContext servletContext) {
-        packages("com.underdog.jersey.spring.example");
+//    public JerseyAppConfiguration(@Context MockServletContext context) {
+        packages("com.underdog.jersey.spring");
         System.out.println("yay at " + Instant.now());
     }
 
--- src/test/java/com/underdog/jersey/spring/impl/test/GreetingResourceIntegrationTest.java
+++ src/test/java/com/underdog/jersey/spring/impl/test/GreetingResourceIntegrationTest.java
@@ -4,12 +4,9 @@ package com.underdog.jersey.spring.impl.test;
 import static org.junit.Assert.assertEquals;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
-import org.glassfish.jersey.server.spring.SpringLifecycleListener;
-import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import com.underdog.jersey.spring.impl.config.JerseyAppConfiguration;
 import com.underdog.jersey.spring.impl.config.SpringAppConfiguration;
 
@@ -21,21 +18,31 @@ public class GreetingResourceIntegrationTest extends JerseyTest {
 
     // For some reason JerseyTest does not invoke the @Context method in the ResourceConfig
 
+
+
     @Override
     public Application configure() {
-        JerseyAppConfiguration config = new JerseyAppConfiguration();
-        ConfigurableApplicationContext context
-                = new AnnotationConfigApplicationContext(SpringAppConfiguration.class);
-        context.getEnvironment().setActiveProfiles("production");
-        config.register(SpringLifecycleListener.class).register(RequestContextFilter.class);
+// OLD
+//        JerseyAppConfiguration config = new JerseyAppConfiguration();
+//        AnnotationConfigApplicationContext context
+//                = new AnnotationConfigApplicationContext(SpringAppConfiguration.class);
+//        context.getEnvironment().setActiveProfiles("production");
+//        config.property("contextConfig", context);
 
+        // NEW
+        AnnotationConfigWebApplicationContext context
+                = new AnnotationConfigWebApplicationContext();
+        context.register(SpringAppConfiguration.class);
+        context.getEnvironment().setActiveProfiles("production");
+        context.refresh();
 //        MockServletContext mockServletContext = new MockServletContext();
-//        GenericWebApplicationContext genericWebApplicationContext = new GenericWebApplicationContext(mockServletContext);
-//        mockServletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, genericWebApplicationContext);
-//        genericWebApplicationContext.setParent(context);
-//        genericWebApplicationContext.refresh();
-        config.property("contextConfig", context);
-//        config.post_construct(mockServletContext);
+//        mockServletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
+//
+        JerseyAppConfiguration config = new JerseyAppConfiguration();
+//        config.register(SpringLifecycleListener.class).register(RequestContextFilter.class);
+//
+//
+        config.property("contextConfig", context); // from SpringComponentProvider.PARAM_SPRING_CONTEXT="contextConfig"
         return config;
     }
 
