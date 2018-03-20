4ba9dce446ac1d380ef7cb0accff8ad1c8a88219
sperez <sperez@meaningcloud.com>
2018-03-20 15:49:10 +0100

    Added fixes

--- src/main/java/com/underdog/jersey/spring/api/resource/GreetingResource.java
+++ src/main/java/com/underdog/jersey/spring/api/resource/GreetingResource.java
@@ -4,9 +4,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import javax.validation.constraints.NotNull;
 
 @Path("/greeting")
 public interface GreetingResource {
     @GET
-    Response getGreeting(@QueryParam("name") String name);
+    Response getGreeting(@NotNull @QueryParam("name") String name);
 }
--- src/main/java/com/underdog/jersey/spring/impl/resource/GreetingResourceImpl.java
+++ src/main/java/com/underdog/jersey/spring/impl/resource/GreetingResourceImpl.java
@@ -3,6 +3,7 @@ package com.underdog.jersey.spring.impl.resource;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -17,7 +18,7 @@ public class GreetingResourceImpl implements GreetingResource {
     private GreetingService greetingService;
 
     @Override
-    public Response getGreeting(@NotNull String name) {
+    public Response getGreeting(@NotNull @QueryParam("name") String name) {
         String greeting = greetingService.getGreeting(name);
         return Response.ok(greeting).build();
     }
